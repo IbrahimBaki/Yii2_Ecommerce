20ab788ece2981178ff19dae75196fb453842680
IbrahimBaki <3baa2y@gmail.com>
2021-01-06 15:23:30 +0200

    Customize Product form

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -53,7 +53,7 @@ AppAsset::register($this);
 
         <!-- Nav Item - Charts -->
         <li class="nav-item">
-            <a class="nav-link" href="charts.html">
+            <a class="nav-link" href="<?php echo \yii\helpers\Url::to(['/product/index']) ?>">
                 <i class="fas fa-fw fa-chart-area"></i>
                 <span>Products</span></a>
         </li>
@@ -292,9 +292,9 @@ AppAsset::register($this);
 
             </nav>
             <!-- End of Topbar -->
-
-            <?php echo $content ?>
-
+            <div class="p-4">
+                <?php echo $content ?>
+            </div>
         </div>
         <!-- End of Main Content -->
 
---
+++ backend/views/product/_form.php
@@ -0,0 +1,48 @@
+<?php
+
+use dosamigos\ckeditor\CKEditor;
+use yii\helpers\Html;
+use yii\bootstrap4\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Product */
+/* @var $form yii\bootstrap4\ActiveForm */
+?>
+
+<div class="product-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->widget(CKEditor::className(), [
+        'options' => ['rows' => 6],
+        'preset' => 'basic'
+    ]) ?>
+
+    <?= $form->field($model, 'image',[
+            'template'=>'
+                <div class="custom-file">
+                {input}
+                {label}
+                {error}
+                </div>
+            ',
+        'labelOptions' =>['class'=>'custom-file-label'],
+        'inputOptions' =>['class'=>'custom-file-input'],
+    ])->textInput(['type'=>'file']) ?>
+
+    <?= $form->field($model, 'price')->textInput([
+        'maxlength' => true,
+        'type'=>'number',
+    ]) ?>
+
+    <?= $form->field($model, 'status')->checkbox() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
