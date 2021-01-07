44d6697616f0a24361f2a99997135f0fc43217eb
IbrahimBaki <3baa2y@gmail.com>
2021-01-07 21:14:59 +0200

    Fix Image Update bug

--- common/models/Product.php
+++ common/models/Product.php
@@ -143,15 +143,15 @@ class Product extends \yii\db\ActiveRecord
         }
         $transaction = Yii::$app->db->beginTransaction();
         $ok =  parent::save($runValidation, $attributeNames);
-        if($ok){
+        if($ok && $this->imageFile){
             $fullPath = Yii::getAlias('@frontend/web/storage'.$this->image);
             $dir = dirname($fullPath);
             if(!FileHelper::createDirectory($dir) | !$this->imageFile->saveAs($fullPath)){
                 $transaction->rollBack();
                 return false;
             }
-            $transaction->commit();
         }
+        $transaction->commit();
         return $ok;
     }
 
