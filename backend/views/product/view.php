ffc3c4ce35446f9ff4a5441a5afdddf3ecc1bd38
IbrahimBaki <3baa2y@gmail.com>
2021-01-07 21:12:36 +0200

    Customize Product View in backend

--- backend/views/product/view.php
+++ backend/views/product/view.php
@@ -30,15 +30,31 @@ $this->params['breadcrumbs'][] = $this->title;
         'model' => $model,
         'attributes' => [
             'id',
+            [
+                'attribute' => 'image',
+                'format' => 'html',
+                'value' => fn() => Html::img($model->getImageUrl(), ['style' => 'width:50px']),
+            ],
             'name',
-            'description:ntext',
-            'image',
-            'price',
-            'status',
-            'created_at',
-            'updated_at',
-            'created_by',
-            'updated_by',
+            'price:currency',
+            [
+                'attribute' => 'status',
+                'format' => 'html',
+                'value' => fn() => Html::tag('span', $model->status ? 'Active' : 'Draft', [
+                    'class' => $model->status ? 'badge badge-success' : 'badge badge-danger'
+                ]),
+
+            ],
+            'description:html',
+            'created_at:datetime',
+            'updated_at:datetime',
+            ['attribute' => 'createdBy.username',
+                'label' => 'Created By'
+            ],
+            [
+                'attribute' => 'updatedBy.username',
+                'label' => 'Updated By'
+            ],
         ],
     ]) ?>
 
