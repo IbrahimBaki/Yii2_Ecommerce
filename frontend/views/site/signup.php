8cd5487cb63d856521dcfea856630e8d215b5b42
IbrahimBaki <3baa2y@gmail.com>
2021-01-09 01:42:11 +0200

    Fix bootstrap3 problems on frontend side

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -1,22 +1,21 @@
 <?php
 
 /* @var $this yii\web\View */
-/* @var $form yii\bootstrap\ActiveForm */
+/* @var $form yii\bootstrap4\ActiveForm */
 /* @var $model \frontend\models\SignupForm */
 
 use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
+use yii\bootstrap4\ActiveForm;
 
 $this->title = 'Signup';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-signup">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="row justify-content-center">
+        <div class="col-lg-6">
+            <h1><?= Html::encode($this->title) ?></h1>
+            <p>Please fill out the following fields to signup:</p>
 
-    <p>Please fill out the following fields to signup:</p>
-
-    <div class="row">
-        <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
 
                 <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
