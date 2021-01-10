b235ffbdab51af33882c72e63adbc83366890212
IbrahimBaki <3baa2y@gmail.com>
2021-01-10 12:27:40 +0200

    Add item to cart using ajax

---
+++ frontend/web/js/app.js
@@ -0,0 +1,21 @@
+
+
+$(function(){
+    const $cartQuantity = $('#cart-quantity');
+    const $addToCart = $('.btn-add-to-cart');
+    $addToCart.click(ev =>{
+        ev.preventDefault();
+        const $this = $(ev.target);
+       const id = $this.closest('.product-item').data('key');
+       console.log(id);
+       $.ajax({
+           method:'POST',
+           url:$this.attr('href'),
+           data:{id},
+           success:function (){
+               console.log(arguments)
+               $cartQuantity.text(parseInt($cartQuantity.text()|| 0) + 1)
+           }
+       })
+    })
+});
