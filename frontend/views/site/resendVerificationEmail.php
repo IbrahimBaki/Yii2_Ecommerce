5a1762eaf668ce3baabd4cd2cf827464ba2cb5e5
IbrahimBaki <3baa2y@gmail.com>
2021-01-12 15:41:21 +0200

    Fix bootstrap 3 bugs

--- frontend/views/site/resendVerificationEmail.php
+++ frontend/views/site/resendVerificationEmail.php
@@ -1,30 +1,30 @@
 <?php
 /* @var $this yii\web\View */
-/* @var $form yii\bootstrap\ActiveForm */
+/* @var $form yii\bootstrap4\ActiveForm */
+
 /* @var $model \frontend\models\ResetPasswordForm */
 
 use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
+use yii\bootstrap4\ActiveForm;
 
 $this->title = 'Resend verification email';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-resend-verification-email">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="row justify-content-center">
+        <div class="col col-lg-5">
+            <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out your email. A verification email will be sent there.</p>
+            <p>Please fill out your email. A verification email will be sent there.</p>
+                    <?php $form = ActiveForm::begin(['id' => 'resend-verification-email-form']); ?>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'resend-verification-email-form']); ?>
+                    <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
 
-            <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
+                    <div class="form-group">
+                        <?= Html::submitButton('Send', ['class' => 'btn btn-primary']) ?>
+                    </div>
 
-            <div class="form-group">
-                <?= Html::submitButton('Send', ['class' => 'btn btn-primary']) ?>
-            </div>
-
-            <?php ActiveForm::end(); ?>
+                    <?php ActiveForm::end(); ?>
         </div>
     </div>
 </div>
