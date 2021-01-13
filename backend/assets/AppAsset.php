28f58d2479f3d619eeda3acaa595cc237a4d9ef8
IbrahimBaki <3baa2y@gmail.com>
2021-01-13 07:10:09 +0200

    define custom css to sort data in order table

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -19,6 +19,7 @@ class AppAsset extends AssetBundle
         'vendor/fontawesome-free/css/all.min.css',
         'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i',
         'css/sb-admin-2.min.css',
+        'css/style.css'
     ];
     public $js = [
         "vendor/jquery-easing/jquery.easing.min.js",
